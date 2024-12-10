bd390f526cb002b1a32009e1f0311eccc30f59b9
hayoung <hachu.developer@gmail.com>
2024-12-10 16:46:00 +0900

    [Update]  home, about 화면 추가

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  pageTogle: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  navigateTo(path: string = ''): void {
+    this.pageTogle = !this.pageTogle;
+    const paths = ['/', 'photos'];
+    this.router.navigate([`./${paths[Number(this.pageTogle)]}`], { relativeTo: this.route });
+  }  
 }
