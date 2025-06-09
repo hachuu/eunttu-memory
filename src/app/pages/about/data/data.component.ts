9db8faaf7d4e3cdaed15c4632e53b1d9f9343091
Hachu <65321592+hachuu@users.noreply.github.com>
2025-06-09 13:39:58 +0900

    Add design placeholders for home and about pages

--- src/app/pages/about/data/data.component.ts
+++ src/app/pages/about/data/data.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './data.component.scss'
 })
 export class DataComponent {
-
+  infoItems = [
+    { key: 'Age', value: '0 years old' },
+    { key: 'Height', value: '74cm' },
+    { key: 'Weight', value: '9.6kg' },
+    { key: 'Blood Type', value: 'A' },
+    { key: 'Favorite Toy', value: 'Sample toy' },
+  ];
 }
