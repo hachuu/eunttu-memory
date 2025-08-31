4c88d17ceb1c47dc382dc5b6fc44cd14032eaf7a
hayoung <hachu.developer@gmail.com>
2025-08-31 20:24:52 +0900

    [Update] 돌끝맘...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent {
   features = [
     { title: 'First Word', desc: 'Dad, Mom, Mamma' },
     { title: 'First Steps', desc: 'Two footprints' },
-    { title: 'First Birthday', desc: 'Waiting' },
+    { title: 'First Birthday', desc: 'The party was a success' },
+    { title: 'First Word', desc: 'KKAKKA' },
   ];
 }
